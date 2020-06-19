c266ef64f288ccb222c18da6b386fc67ab31c816
CHOI-YOOBIN <cyb9701@gmail.com>
2020-06-20 05:07:38 +0900

    - Instagram App Clone

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutterinstagramclone/pages/main_page.dart';
 
 void main() => runApp(MyApp());
 
-bool isItFirstData = true;
+bool isItFirstData = false;
 
 class MyApp extends StatelessWidget {
   @override
