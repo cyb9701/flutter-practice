757bb2fe4ccd33d77cb2adef6bc1dad05d616ce2
BillyoDev <59527787+CHOI-YOOBIN@users.noreply.github.com>
2022-01-13 22:14:10 +0900

    월 주차 구하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +9,176 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('ko', 'KR'),
+      ],
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // 선택한 날짜.
+  DateTime? _selectedDate;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // 월 주차.
+  int? _weekOfMonth;
+
+  // date picker 열기.
+  Future<void> showDatePickerPop() async {
+    final DateTime? _pickerDate = await showDatePicker(
+      context: context,
+      locale: const Locale('ko', 'KR'),
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2030),
+    );
+
+    if (_pickerDate != null) {
+      setState(() {
+        _selectedDate = _pickerDate;
+        _weekOfMonth = weekOfMonthForStandard(_pickerDate);
+      });
+    }
+  }
+
+  // 월 주차. (정식 규정에 따라서 계산)
+  int weekOfMonthForStandard(DateTime date) {
+    // 월 주차.
+    late int _weekOfMonth;
+
+    // 선택한 월의 첫번째 날짜.
+    final _firstDay = DateTime(date.year, date.month, 1);
+
+    // 선택한 월의 마지막 날짜.
+    final _lastDay = DateTime(date.year, date.month + 1, 0);
+
+    // 첫번째 날짜가 목요일보다 작은지 판단.
+    final _isFirstDayBeforeThursday = _firstDay.weekday <= DateTime.thursday;
+
+    // 선택한 날짜와 첫번째 날짜가 같은 주에 위치하는지 판단.
+    if (isSameWeek(date, _firstDay)) {
+      // 첫번째 날짜가 목요일보다 작은지 판단.
+      if (_isFirstDayBeforeThursday) {
+        // 1주차.
+        _weekOfMonth = 1;
+      }
+
+      // 저번달의 마지막 날짜의 주차와 동일.
+      else {
+        final _lastDayOfPreviousMonth = DateTime(date.year, date.month, 0);
+
+        // n주차.
+        _weekOfMonth = weekOfMonthForStandard(_lastDayOfPreviousMonth);
+      }
+    } else {
+      // 선택한 날짜와 마지막 날짜가 같은 주에 위치하는지 판단.
+      if (isSameWeek(date, _lastDay)) {
+        // 마지막 날짜가 목요일보다 큰지 판단.
+        final _isLastDayBeforeThursday = _lastDay.weekday >= DateTime.thursday;
+        if (_isLastDayBeforeThursday) {
+          // 주차를 단순 계산 후 첫번째 날짜의 위치에 따라서 0/-1 결합.
+          // n주차.
+          _weekOfMonth = weekOfMonthForSimple(date) + (_isFirstDayBeforeThursday ? 0 : -1);
+        }
+
+        // 다음달 첫번째 날짜의 주차와 동일.
+        else {
+          // 1주차.
+          _weekOfMonth = 1;
+        }
+      }
+
+      // 첫번째주와 마지막주가 아닌 날짜들.
+      else {
+        // 주차를 단순 계산 후 첫번째 날짜의 위치에 따라서 0/-1 결합.
+        // n주차.
+        _weekOfMonth = weekOfMonthForSimple(date) + (_isFirstDayBeforeThursday ? 0 : -1);
+      }
+    }
+
+    return _weekOfMonth;
+  }
+
+  // 월 주차. (단순하게 1일이 1주차 시작).
+  int weekOfMonthForSimple(DateTime date) {
+    // 월의 첫번째 날짜.
+    DateTime _firstDay = DateTime(date.year, date.month, 1);
+
+    // 월중에 첫번째 월요일인 날짜.
+    DateTime _firstMonday = _firstDay.add(Duration(days: (DateTime.monday + 7 - _firstDay.weekday) % 7));
+
+    // 첫번째 날짜와 첫번째 월요일인 날짜가 동일한지 판단.
+    // 동일할 경우: 1, 동일하지 않은 경우: 2 를 마지막에 더한다.
+    final bool isFirstDayMonday = _firstDay == _firstMonday;
+
+    final _different = calculateDaysBetween(from: _firstMonday, to: date);
+
+    // 주차 계산.
+    int _weekOfMonth = (_different / 7 + (isFirstDayMonday ? 1 : 2)).toInt();
+    return _weekOfMonth;
+  }
+
+  // D-Day 계산.
+  int calculateDaysBetween({required DateTime from, required DateTime to}) {
+    return (to.difference(from).inHours / 24).round();
+  }
+
+  // 동일한 주차인지 확인.
+  bool isSameWeek(DateTime dateTime1, DateTime dateTime2) {
+    final int _dateTime1WeekOfMonth = weekOfMonthForSimple(dateTime1);
+    final int _dateTime2WeekOfMonth = weekOfMonthForSimple(dateTime2);
+    return _dateTime1WeekOfMonth == _dateTime2WeekOfMonth;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // 날짜 선택 버튼.
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.center,
+                child: OutlinedButton(
+                  onPressed: () async => showDatePickerPop(),
+                  child: const Text('날짜 선택'),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+
+            // 선탁한 날짜.
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('선택한 날짜: ${_selectedDate ?? ''}'),
+            ),
+
+            // 주차.
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('월 주차: ${_weekOfMonth ?? ''}'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
