240b31ce7d9c3295d948a5b4f2375bcc4ba003ed
BillyoDev <59527787+CHOI-YOOBIN@users.noreply.github.com>
2022-01-22 15:25:09 +0900

    토스 로고
    토스 로고의 그라데이션 효과 변경

--- lib/widgets/toss_logo.dart
+++ lib/widgets/toss_logo.dart
@@ -7,19 +7,22 @@ class TossLogo extends StatelessWidget {
   Widget build(BuildContext context) {
     return ShaderMask(
       blendMode: BlendMode.srcIn,
-      shaderCallback: (bounds) => const LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          Colors.white,
-          Colors.black12,
-        ],
-      ).createShader(
-        Rect.fromCircle(
-          center: Offset(bounds.width, bounds.height / 2),
-          radius: 30,
-        ),
-      ),
+      shaderCallback: (bounds) {
+        return const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.white38,
+            Colors.black38,
+          ],
+        ).createShader(
+          Rect.fromCenter(
+            center: Offset(bounds.width / 2, bounds.height * 0.7),
+            width: bounds.width,
+            height: bounds.height,
+          ),
+        );
+      },
       child: const Text(
         'toss bank',
         style: TextStyle(
