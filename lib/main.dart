50ab81e50ac666e793993660d707cebd42c83102
BillyoDev <59527787+CHOI-YOOBIN@users.noreply.github.com>
2022-01-28 22:52:18 +0900

    불필요한 코드 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData.dark(),
       home: const GiftCardListPage(),
     );
   }
-}dsfsdfdsfsdfsdf
+}
