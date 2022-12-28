e0afd161e18e603384236c7447208315df215d96
cyb9701 <cyb9701@gmail.com>
2022-12-28 16:08:28 +0900

    스크롤 방향에 따라 앱바의 애니메이션 상태가 변경되는 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +8,105 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const AnimationAppBarPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class AnimationAppBarPage extends StatefulWidget {
+  const AnimationAppBarPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AnimationAppBarPage> createState() => _AnimationAppBarPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _AnimationAppBarPageState extends State<AnimationAppBarPage> {
+  /// 스크롤 컨트롤러.
+  late ScrollController _scrollController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  /// 앱바의 노출 여부 확인 변수.
+  bool _exposureAppBar = true;
+
+  /// 스크롤 방향에 따른 앱바 노출 여부 상태 변경.
+  void _changeExposureAppBarState() {
+    try {
+      if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+        if (_exposureAppBar) {
+          setState(() {
+            _exposureAppBar = false;
+          });
+        }
+      } else if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+        if (!_exposureAppBar) {
+          setState(() {
+            _exposureAppBar = true;
+          });
+        }
+      }
+    } catch (_) {}
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController()..addListener(_changeExposureAppBarState);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      appBar: _animationAppBar(),
+      body: _body(),
+    );
+  }
+
+  /// 애니메이션 앱바.
+  PreferredSize _animationAppBar() {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(48),
+      child: SafeArea(
+        child: AnimatedCrossFade(
+          firstChild: AppBar(
+            leading: const Icon(Icons.arrow_back_ios_new_rounded),
+            backgroundColor: Colors.blueAccent,
+            title: const Text('앱바'),
+          ),
+          secondChild: const SizedBox.shrink(),
+          crossFadeState: _exposureAppBar ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+          duration: const Duration(milliseconds: 200),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  /// 리스트뷰.
+  ListView _body() {
+    return ListView.separated(
+      controller: _scrollController,
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        return Container(
+          height: 50,
+          color: Colors.grey[700],
+          child: Center(
+            child: Text(
+              index.toString(),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (_, __) => const SizedBox(height: 10),
     );
   }
 }
